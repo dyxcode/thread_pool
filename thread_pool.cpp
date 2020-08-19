9dc4feb348721e75640a5aae8b149be0f041f65e
dyxcode <821440651@qq.com>
2020-08-19 21:23:12 +0800

    update thread_pool

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -5,82 +5,50 @@ ThreadPool::ThreadPool(std::size_t thread_num) :
 {
 	while (thread_num--) {
 		//事件循环，用lambda打包提交到thread中
-		threads_.emplace_back(std::thread([this] {
+		threads_.emplace_back([this] {
 			//循环外加锁，不是每次都对循环内的临界区加锁，而是对非临界区解锁
 			Ulock u_guard{this->mtx_};
 			while (true) {
-				if (!this->tasks_.empty()) { //任务队列非空，则领取任务
-					auto task{std::move(this->tasks_.front())};
-					this->tasks_.pop();
-					//执行任务时解锁
-					u_guard.unlock();
-					task();
-					u_guard.lock();
-				} else if (!this->is_start_ && !this->timer_thread_.joinable()) { //线程池中止，线程退出
+				if (!this->tasks_.empty()) { //任务队列非空
+					CTimePoint& now = std::chrono::steady_clock::now();
+					CTimePoint& execute_time = std::get<0>(this->tasks_.cbegin()->first);
+					if (now >= execute_time) {
+						auto task{std::move(*(this->tasks_.begin()))};
+						this->tasks_.erase(this->tasks_.begin());
+						//执行任务时解锁
+						u_guard.unlock();
+						if (std::get<0>(task.first) != std::get<2>(task.first)) {
+							auto period = std::get<2>(task.first) - std::get<0>(task.first);
+							CTimePoint& execute_time = std::get<2>(task.first);
+							CTimePoint& period_time = execute_time + period;
+							this->addTask(std::make_tuple(execute_time, now, period_time), task.second);
+						}
+						task.second();
+						u_guard.lock();
+					} else {
+						this->cv_.wait_until(u_guard, execute_time);
+					}
+				} else if (!this->is_start_) { //线程池中止，线程退出
 					break;
 				} else { //等待任务队列出现任务，或者线程池中止
 					this->cv_.wait(u_guard);
 				}
 			}
-		}));
+		});
 	}
-	// start a timer thread
-	timer_thread_ = std::move(std::thread([this] {
-		Ulock u_guard{this->timer_mtx_};
-		while (true) {
-			if (!this->timer_tasks_.empty()) {
-				TimePoint now = std::chrono::steady_clock::now();
-				if (now >= this->timer_tasks_.top().second) {
-					this->execute(std::move(this->timer_tasks_.top().first));
-					this->timer_tasks_.pop();
-				} else
-					this->timer_cv_.wait_until(u_guard, this->timer_tasks_.top().second);
-			} else {
-				{
-					Lockgd guard{this->mtx_};
-					if (!this->is_start_) break;
-				}
-				this->timer_cv_.wait(u_guard);
-			}
-		}
-	}));
 }
 
-// 等待所有线程关闭，并销毁所有线程
-void ThreadPool::wait() {
+ThreadPool::~ThreadPool() {
 	{ // 对于is_start的修改和工作线程对于is_start的读取要互斥
 		Lockgd guard{mtx_};
-		if (!is_start_) return;
 		is_start_ = false;
 	}
-	timer_cv_.notify_all();
-	timer_thread_.join();
-
 	cv_.notify_all();
 	// 回收所有线程
 	for (auto && item : threads_)
 		item.join();
 }
 
-// 添加一个任务
-inline void ThreadPool::execute(std::function<void()> task) {
-	{
-		Lockgd guard{mtx_};
-		tasks_.push(std::move(task));
-	}
-	cv_.notify_one();
-}
-
-// 添加一个定时任务
-inline void ThreadPool::execute(std::function<void()> task, std::size_t milliseconds) {
-	TimePoint tp = std::chrono::steady_clock::now() + std::chrono::milliseconds(milliseconds);
-	{
-		Lockgd guard{timer_mtx_};
-		timer_tasks_.push({std::move(task), tp});
-	}
-	timer_cv_.notify_all();
-}
-
-ThreadPool::~ThreadPool() {
-	wait();
+bool ThreadPool::cancel(const Task_Handle& task_handle) {
+	return tasks_.erase(task_handle);
 }
--- thread_pool.h
+++ thread_pool.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#include <queue>
+#include <map>
+#include <vector>
 #include <condition_variable>
 
 class ThreadPool{
+	template<typename V, typename R>
+	using CDuration = const std::chrono::duration<V, R>;
+	using CTimePoint = const std::chrono::steady_clock::time_point;
+	// 执行时间，提交时间，周期时间
+	using Task_Handle = std::tuple<CTimePoint, CTimePoint, CTimePoint>;
 	using Lockgd = std::lock_guard<std::mutex>;
 	using Ulock = std::unique_lock<std::mutex>;
-	using TimePoint = std::chrono::steady_clock::time_point;
-	using TimerTask = std::pair<std::function<void()>, TimePoint>;
 
 public:
 	explicit ThreadPool(std::size_t thread_num);
@@ -18,30 +22,49 @@ public:
 	ThreadPool(const ThreadPool&) = delete;
 	ThreadPool& operator=(const ThreadPool&) = delete;
 
-	// 等待所有线程关闭，并销毁所有线程
-	void wait();
-
-	// 添加一个任务
-	void execute(std::function<void()> task);
+	// 添加一个普通任务
+	template<typename F>
+	void execute(F&& task) {
+		CTimePoint& now = std::chrono::steady_clock::now();
+		addTask(std::make_tuple(now, now, now), std::forward<F>(task));
+	}
 
 	// 添加一个定时任务
-	void execute(std::function<void()> task, std::size_t milliseconds);
+	template<typename F, typename V, typename R>
+	Task_Handle execute(F&& task, CDuration<V, R>& duration) {
+		CTimePoint& execute_time = std::chrono::steady_clock::now() + duration;
+		CTimePoint& now = std::chrono::steady_clock::now();
+		auto task_handle = make_tuple(execute_time, now, execute_time);
+		addTask(task_handle, std::forward<F>(task));
+		return task_handle;
+	}
+
+	// 添加一个周期任务
+	template<typename F, typename V1, typename R1, typename V2, typename R2>
+	Task_Handle execute(F&& task, CDuration<V1, R1>& duration, CDuration<V2, R2>& period) {
+		CTimePoint& execute_time = std::chrono::steady_clock::now() + duration;
+		CTimePoint& period_time = execute_time + period;
+		CTimePoint& now = std::chrono::steady_clock::now();
+		auto task_handle = make_tuple(execute_time, now, period_time);
+		addTask(task_handle, std::forward<F>(task));
+		return task_handle;
+	}
+
+	bool cancel(const Task_Handle& task_handle);
 
 private:
-	struct Compare_TimerTask {
-		bool operator()(const TimerTask& lhs, const TimerTask& rhs) {
-			return std::greater<TimePoint>()(lhs.second, rhs.second);
+	template<typename F>
+	void addTask(const Task_Handle& task_handle, F&& task) {
+		{
+			Lockgd guard{mtx_};
+			tasks_.emplace(task_handle, std::forward<F>(task));
 		}
-	};
+		cv_.notify_one();
+	}
 
 	bool is_start_;
 	std::mutex mtx_;
 	std::condition_variable cv_;
 	std::vector<std::thread> threads_;
-	std::queue<std::function<void()>> tasks_;
-
-	std::mutex timer_mtx_;
-	std::condition_variable timer_cv_;
-	std::thread timer_thread_;
-	std::priority_queue<TimerTask, std::deque<TimerTask>, Compare_TimerTask> timer_tasks_;
+	std::map<Task_Handle, std::function<void()>> tasks_;
 };
