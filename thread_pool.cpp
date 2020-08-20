872b709fcd9ed10be452e8eb099602cd772e42c2
dyxcode <821440651@qq.com>
2020-08-20 17:44:39 +0800

    update thread_pool

--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,7 +1,11 @@
 #include "thread_pool.h"
+#include <iostream>
+
+std::atomic<size_t> ThreadPool::task_num_{0};
 
 ThreadPool::ThreadPool(std::size_t thread_num) :
-	is_start_(true)
+	is_start_(true),
+	is_wait_(false)
 {
 	while (thread_num--) {
 		//事件循环，用lambda打包提交到thread中
@@ -10,23 +14,28 @@ ThreadPool::ThreadPool(std::size_t thread_num) :
 			Ulock u_guard{this->mtx_};
 			while (true) {
 				if (!this->tasks_.empty()) { //任务队列非空
-					CTimePoint& now = std::chrono::steady_clock::now();
-					CTimePoint& execute_time = std::get<0>(this->tasks_.cbegin()->first);
+					const TimePoint& now = std::chrono::steady_clock::now(); // 获取当前时间
+					const TimePoint& execute_time = std::get<0>(*(this->tasks_.cbegin())); //获取第一个任务的执行时间
 					if (now >= execute_time) {
 						auto task{std::move(*(this->tasks_.begin()))};
 						this->tasks_.erase(this->tasks_.begin());
 						//执行任务时解锁
-						u_guard.unlock();
-						if (std::get<0>(task.first) != std::get<2>(task.first)) {
-							auto period = std::get<2>(task.first) - std::get<0>(task.first);
-							CTimePoint& execute_time = std::get<2>(task.first);
-							CTimePoint& period_time = execute_time + period;
-							this->addTask(std::make_tuple(execute_time, now, period_time), task.second);
+						if (std::get<0>(task) == std::get<1>(task)) { // 如果不是周期任务
+							this->task_indexes_.erase(std::get<3>(task));
+							u_guard.unlock();
+						} else {
+							u_guard.unlock();
+							auto period = std::get<1>(task) - std::get<0>(task); //获取周期
+							std::get<0>(task) = std::get<1>(task);               //设置下一次执行时间
+							std::get<1>(task) = std::get<0>(task) + period;		 //设置下下次执行时间
+							this->addTask<true>(task);
 						}
-						task.second();
+						std::get<2>(task)();
 						u_guard.lock();
-					} else {
+					} else if (!this->is_wait_) {
+						this->is_wait_ = true;
 						this->cv_.wait_until(u_guard, execute_time);
+						this->is_wait_ = false;
 					}
 				} else if (!this->is_start_) { //线程池中止，线程退出
 					break;
@@ -49,6 +58,19 @@ ThreadPool::~ThreadPool() {
 		item.join();
 }
 
-bool ThreadPool::cancel(const Task_Handle& task_handle) {
-	return tasks_.erase(task_handle);
+bool ThreadPool::cancel(std::size_t task_index)
+{
+	Lockgd guard{mtx_};
+	auto task_handle = task_indexes_.find(task_index);
+	if (task_handle != task_indexes_.end()) {
+		tasks_.erase(task_handle->second);
+		task_indexes_.erase(task_handle);
+		return true;
+	}
+	return false;
+}
+
+size_t ThreadPool::getTaskIndex()
+{
+	return static_cast<size_t>(std::chrono::steady_clock::now().time_since_epoch().count() << 8) + (++task_num_ % (1 << 8));
 }
--- thread_pool.h
+++ thread_pool.h
@@ -1,20 +1,28 @@
 #pragma once
 
-#include <map>
+#include <set>
 #include <vector>
+#include <unordered_map>
+#include <atomic>
 #include <condition_variable>
 
 class ThreadPool{
 	template<typename V, typename R>
 	using CDuration = const std::chrono::duration<V, R>;
-	using CTimePoint = const std::chrono::steady_clock::time_point;
+	using TimePoint = std::chrono::steady_clock::time_point;
 	using Lockgd = std::lock_guard<std::mutex>;
 	using Ulock = std::unique_lock<std::mutex>;
 
-public:
-	// 执行时间，提交时间，周期时间
-	using Task_Handle = std::tuple<CTimePoint, CTimePoint, CTimePoint>;
+	// 执行时间,周期时间,执行函数,句柄索引
+	using TaskType = std::tuple<TimePoint, TimePoint, std::function<void()>, std::size_t>;
+	struct CompareTask {
+		bool operator()(const TaskType& lhs, const TaskType& rhs) {
+			return std::less<TimePoint>()(std::get<0>(lhs), std::get<0>(rhs));
+		}
+	};
+	using TaskHandle = std::multiset<TaskType, CompareTask>::iterator;
 
+public:
 	explicit ThreadPool(std::size_t thread_num);
 
 	~ThreadPool();
@@ -26,46 +34,58 @@ public:
 	// 添加一个普通任务
 	template<typename F>
 	void execute(F&& task) {
-		CTimePoint& now = std::chrono::steady_clock::now();
-		addTask(std::make_tuple(now, now, now), std::forward<F>(task));
+		const TimePoint& now = std::chrono::steady_clock::now();
+		addTask<false>(std::make_tuple(now, now, std::forward<F>(task), getTaskIndex()));
 	}
 
-	// 添加一个定时任务
+	// 添加一个延时任务
 	template<typename F, typename V, typename R>
-	Task_Handle execute(F&& task, CDuration<V, R>& duration) {
-		CTimePoint& execute_time = std::chrono::steady_clock::now() + duration;
-		CTimePoint& now = std::chrono::steady_clock::now();
-		auto task_handle = make_tuple(execute_time, now, execute_time);
-		addTask(task_handle, std::forward<F>(task));
-		return task_handle;
+	std::size_t execute(F&& task, CDuration<V, R>& duration) {
+		const TimePoint& execute_time = std::chrono::steady_clock::now() + duration;
+		std::size_t task_index = getTaskIndex();
+		addTask<true>(std::make_tuple(execute_time, execute_time, std::forward<F>(task), task_index));
+		return task_index;
 	}
 
 	// 添加一个周期任务
 	template<typename F, typename V1, typename R1, typename V2, typename R2>
-	Task_Handle execute(F&& task, CDuration<V1, R1>& duration, CDuration<V2, R2>& period) {
-		CTimePoint& execute_time = std::chrono::steady_clock::now() + duration;
-		CTimePoint& period_time = execute_time + period;
-		CTimePoint& now = std::chrono::steady_clock::now();
-		auto task_handle = make_tuple(execute_time, now, period_time);
-		addTask(task_handle, std::forward<F>(task));
-		return task_handle;
+	std::size_t execute(F&& task, CDuration<V1, R1>& duration, CDuration<V2, R2>& period) {
+		const TimePoint& execute_time = std::chrono::steady_clock::now() + duration;
+		const TimePoint& period_time = execute_time + period;
+		std::size_t task_index = getTaskIndex();
+		addTask<true>(std::make_tuple(execute_time, period_time, std::forward<F>(task), task_index));
+		return task_index;
 	}
 
-	bool cancel(const Task_Handle& task_handle);
+	bool cancel(std::size_t task_index);
 
 private:
-	template<typename F>
-	void addTask(const Task_Handle& task_handle, F&& task) {
+	size_t getTaskIndex();
+
+	template<bool set_index> 
+	void addTask(const TaskType& task) {
 		{
 			Lockgd guard{mtx_};
-			tasks_.emplace(task_handle, std::forward<F>(task));
+			task_indexes_[std::get<3>(task)] = tasks_.emplace(task); // 存储任务索引和指向任务的指针
+		}
+		cv_.notify_one();
+	}
+	template<> 
+	void addTask<false>(const TaskType& task) {
+		{
+			Lockgd guard{mtx_};
+			tasks_.emplace(task);
 		}
 		cv_.notify_one();
 	}
 
-	bool is_start_;
-	std::mutex mtx_;
-	std::condition_variable cv_;
-	std::vector<std::thread> threads_;
-	std::map<Task_Handle, std::function<void()>> tasks_;
+	static std::atomic<size_t> task_num_;	// 记录任务序号，用于生成索引
+
+	bool is_start_;							// 线程池是否开始运行
+	bool is_wait_;							// 是否有线程等待延时任务
+	std::mutex mtx_;						// 保护以下数据成员的互斥锁
+	std::condition_variable cv_;			// 条件变量
+	std::vector<std::thread> threads_;		// 线程容器
+	std::unordered_map<std::size_t, TaskHandle> task_indexes_;	// 记录任务索引和指向任务的指针
+	std::multiset<TaskType, CompareTask> tasks_;				// 保存任务的容器
 };
